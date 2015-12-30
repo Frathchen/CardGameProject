e5b4d3ed5d18581b0cf8b50f1182dc198983ed71
Franz Krause <Frathkraus@gmail.com>
2015-12-30 08:54:23 +0100

    some example stuff #2

--- core/src/example/Testus.java
+++ core/src/example/Testus.java
@@ -2,7 +2,10 @@ package example;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,12 +15,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Testus extends ApplicationAdapter {
     private Texture dropImage;
     private Texture bucketImage;
+    private Sound dropSound;
+    private Music rainMusic;
 
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
 
     @Override
     public void create () {
         dropImage = new Texture(Gdx.files.internal("droplet.png"));
         bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("Wanderer.mp3"));
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("Regenstaebe.mp3"));
+
+        rainMusic.setLooping(true);
+        rainMusic.play();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 400);
+
+        batch = new SpriteBatch();
+
+
     }
 
     @Override
